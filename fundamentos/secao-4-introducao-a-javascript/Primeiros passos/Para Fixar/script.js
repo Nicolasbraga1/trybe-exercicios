4e8d2a972f248cfc23552f71bb43e9761eb1f961
Nícolas <nicolasbrga01@gmail.com>
2022-09-21 16:27:11 -0300

    aula concluida, agora exercicio

---
+++ fundamentos/secao-4-introducao-a-javascript/Primeiros passos/Para Fixar/script.js
@@ -0,0 +1,18 @@
+const myName = "Nícolas";
+const birthCity = "São João de Meriti";
+let birthYear = 2000;
+birthYear = 2030;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(typeof patientEmail);
+console.log(typeof patientId);
+console.log(typeof patientInfo);
+console.log(typeof isEnrolled);
