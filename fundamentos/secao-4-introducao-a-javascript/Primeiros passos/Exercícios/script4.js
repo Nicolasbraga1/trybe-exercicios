7986958b42c9c3db30d4acf15cf7d48e6fa8d594
Nícolas <nicolasbrga01@gmail.com>
2022-09-21 19:31:54 -0300

    finalmente terminei

---
+++ fundamentos/secao-4-introducao-a-javascript/Primeiros passos/Exercícios/script4.js
@@ -0,0 +1,11 @@
+const ladoA = 50;
+const ladoB = 10;
+const ladoC = 30;
+if (ladoA+ladoB+ladoC===180){
+  console.log(true);
+}
+else if (ladoA+ladoB+ladoC!==180){
+  console.log(false);
+} else {
+  console.log("valor inválido");
+}
